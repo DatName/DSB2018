a0bd8b7b14792c77fef027595b68ce6bba8ffbe5
Pavel <g.korelin@gmail.com>
2018-03-25 22:19:50 +0200

    up

--- scripts/nn.jl
+++ scripts/nn.jl
@@ -6,9 +6,9 @@ using Plots
 
 function form_regressor(this::DSB2018.Data.Image)
     x̂ = Float64.(Gray.(this.image))
-    dens = DSB2018.Model.to_density(x̂, 4)
 
-    grad_y, grad_x, edges, orient = imedge(dens, KernelFactors.ando3, "replicate")
+    dens = DSB2018.Model.to_density(x̂, 4)
+    dens = abs.(dens) / sum(abs.(dens))
 
     adaptive_threshold = DSB2018.Model.skimage_filters.threshold_local(dens, 11, offset=0.0)
     global_threshold   = DSB2018.Model.skimage_filters.threshold_otsu(dens)
@@ -20,6 +20,8 @@ function form_regressor(this::DSB2018.Data.Image)
     img_t = DSB2018.Model.skimage_morphology.remove_small_objects(img_t, min_size=4)
     img_t = DSB2018.Model.ndi.binary_fill_holes(img_t)
     img_f = Float64.(img_t)
+
+    grad_y, grad_x, edges, orient = imedge(img_f, KernelFactors.ando3, "replicate")
 
     dens, img_f, edges
 end
@@ -129,7 +131,7 @@ function get_metric(this::DSB2018.Data.Image, res::Matrix{Float64})
     return metric(prediction_vector, truth_images)
 end
 
-num_images_to_process = 10
+num_images_to_process = 30
 batch_size = (128, 128)
 num_samples_per_image = 10
 
@@ -140,6 +142,8 @@ for k = 1 : num_images_to_process
     img = DSB2018.Data.loadTrainImage(ids[k])
     append!(train_data, form_data(img, batch_size, num_samples_per_image))
 end
+
+all_images = DSB2018.Data.loadTrainImage.(ids)
 
 function loss(this::Tuple) #train_data
     return loss(this[1], this[2], this[3])
@@ -152,11 +156,49 @@ function loss(x, y, w)
     (σ.(z/10) - 0.5) / 0.005*0.02
 end
 
+bx = batch_size[1]
+by = batch_size[2]
+
+struct IMResize2
+    size::Tuple{Int64, Int64}
+end
+
+Flux.treelike(IMResize2)
+function (this::IMResize2)(x)
+    return Flux.Tracker.track(Images.imresize, x, this.size)
+end
+
+import Flux.Tracker.back
+
+function back(::typeof(Images.imresize), Δ, xs::Flux.TrackedArray, _...)
+    back(xs, Images.imresize(Δ, size(xs)))
+end
+
+rz_down1 = IMResize2((88, 88))
+rz_down2 = IMResize2((56, 56))
+
+rz_up1   = IMResize2((88, 88))
+rz_up2   = IMResize2((bx, by))
+
 nn_model = Chain(Flux.Conv((9, 9), 3 => 1, pad = (4, 4), σ),
-                x -> reshape(x, (batch_size[1], batch_size[2])))
+                x -> reshape(x, (bx, by)),
+                x -> rz_down1(x),
+                x -> reshape(x, (88, 88, 1, 1)),
+                Flux.Conv((9, 9), 1 => 1, pad = (4, 4), σ),
+                x -> reshape(x, (88, 88)),
+                x -> rz_down2(x),
+                x -> reshape(x, (56, 56, 1, 1)),
+                Flux.Conv((9, 9), 1 => 1, pad = (4, 4), σ),
+                x -> reshape(x, (56, 56)),
+                x -> rz_up1(x),
+                x -> reshape(x, (88, 88, 1, 1)),
+                Flux.Conv((9, 9), 1 => 1, pad = (4, 4), σ),
+                x -> reshape(x, (88, 88)),
+                x -> rz_up2(x)
+                )
 
 opt = Flux.Nesterov(params(nn_model), 0.2)
-train_percent = 0.5
+train_percent = 0.6
 
 ltest  = Float64[]
 ltrain = Float64[]
@@ -164,13 +206,14 @@ ltrain = Float64[]
 num_train = Int64(round(length(train_data)*train_percent))
 num_test  = length(train_data) - num_train
 
-jtrain = rand(1 : length(train_data), num_train)
+jtrain = unique(rand(1 : length(train_data), num_train))
 jtest  = setdiff(1:length(train_data), jtrain)
 
 check_points = Vector{Flux.Chain}(0)
 
-Juno.@progress "Training" for k = 1 : 200
+Juno.@progress "Training" for k = 1 : 3000
     Flux.train!(loss, train_data[jtrain], opt)
+    @show k
 
     train_loss = mean(loss.(train_data[jtrain]))
     test_loss = mean(loss.(train_data[jtest]))
@@ -179,13 +222,13 @@ Juno.@progress "Training" for k = 1 : 200
     push!(ltest, test_loss.tracker.data)
     push!(check_points, deepcopy(nn_model))
 
-    if mod(k, 10) == 0
-        plt_train = plot(ltrain[10:end],
+    if (mod(k, 10) == 0) || (k == 1)
+        plt_train = plot(ltrain[1:end],
                         title = @sprintf("Train Loss: %2.6f", train_loss),
                         fillrange = [minimum(ltrain) maximum(ltrain)],
                         fillalpha = 0.3)
 
-        plt_valid = plot(ltest[10:end],
+        plt_valid = plot(ltest[1:end],
                         title = @sprintf("Test Loss: %2.6f", test_loss),
                         fillrange = [minimum(ltest) maximum(ltest)],
                         fillalpha = 0.3)
@@ -193,8 +236,6 @@ Juno.@progress "Training" for k = 1 : 200
         display(plot(plt_train, plt_valid, layout = grid(2, 1)))
     end
 end
-
-all_images = DSB2018.Data.loadTrainImage.(ids)
 
 scores = Vector{Float64}(length(all_images))
 Juno.@progress "Metric" for k in eachindex(all_images)
@@ -216,3 +257,5 @@ scatter(scores_prev, title=@sprintf("%2.4f", mean(scores_prev)))
 
 imshow(pred)
 get_metric(img, pred)
+
+x̂ = Float64.(Gray.(img.image))
